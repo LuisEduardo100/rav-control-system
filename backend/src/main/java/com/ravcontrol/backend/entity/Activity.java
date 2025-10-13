714a3f1e396ad648c136e6c0ced97c4d4c1526c9
LuisEduardo100 <luisedu.lepa@gmail.com>
2025-10-12 22:05:59 -0300

    feat(backend): Cria entidade de atividade e grupos com suas relações e colunas

---
+++ backend/src/main/java/com/ravcontrol/backend/entity/Activity.java
@@ -0,0 +1,120 @@
+package com.ravcontrol.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "activities")
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer position = 0;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "CLOB")
+    private String description;
+
+    @Column(name= "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(nullable = false)
+    private Boolean completed = false;
+
+    @Column(
+        name = "created_at",
+        nullable = false,
+        updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", nullable = false)
+    private ActivityGroup group;
+
+    @PrePersist
+    public void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public ActivityGroup getGroup() {
+        return group;
+    }
+
+    protected void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    protected void setGroup(ActivityGroup group) {
+        this.group = group;
+    }
+
+    protected void attachToGroup(ActivityGroup group, int position) {
+        this.group = group;
+        this.position = position;
+    }
+}
