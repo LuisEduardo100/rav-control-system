1f58651fc7b45ae742f133e4c04440f98aa7d727
LuisEduardo100 <luisedu.lepa@gmail.com>
2025-10-13 14:29:11 -0300

    feat(backend): cria error handler para quando recursos não forem encontrado no service

--- backend/src/main/java/com/ravcontrol/backend/service/ActivityGroupService.java
+++ backend/src/main/java/com/ravcontrol/backend/service/ActivityGroupService.java
@@ -1,4 +1,48 @@
 package com.ravcontrol.backend.service;
 
+import com.ravcontrol.backend.dto.activityGroup.request.ActivityGroupRequestDTO;
+import com.ravcontrol.backend.dto.activityGroup.response.ActivityGroupResponseDTO;
+import com.ravcontrol.backend.entity.ActivityGroup;
+import com.ravcontrol.backend.repository.ActivityGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ActivityGroupService {
+    private final ActivityGroupRepository groupRepository;
+
+    @Autowired
+    public ActivityGroupService(ActivityGroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ActivityGroupResponseDTO> findAllGroups() {
+        List<ActivityGroup> groups = groupRepository.findAll();
+        return groups
+            .stream()
+            .map(ActivityGroupResponseDTO::fromEntity) // method reference que substitui o método lambda
+            .collect(Collectors.toList()); // por que Collectors.toList()?
+    }
+
+    @Transactional
+    public ActivityGroupResponseDTO createGroup(ActivityGroupRequestDTO dto) {
+        ActivityGroup newGroup = new ActivityGroup();
+        newGroup.setName(dto.name());
+        ActivityGroup savedGroup = groupRepository.save(newGroup);
+        return ActivityGroupResponseDTO.fromEntity(savedGroup);
+    }
+
+    @Transactional
+    public ActivityGroupResponseDTO updateGroup(Long groupId, ActivityGroupRequestDTO dto) {
+        ActivityGroup group = groupRepository.findById(groupId).orElseThrow(() -> new )
+
+    }
+
+
+
 }
