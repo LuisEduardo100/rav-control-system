dbd39cdc111a601d431bf4ed5635d0802b5d329f
Luís Eduardo de Paula Albuquerque <144605889+LuisEduardo100@users.noreply.github.com>
2025-10-15 16:13:25 -0300

    feat(backend): adiciona position na entidade ActivityGroup e cria forma de ordenar grupos por position. (#36)

--- backend/src/main/java/com/ravcontrol/backend/repository/ActivityGroupRepository.java
+++ backend/src/main/java/com/ravcontrol/backend/repository/ActivityGroupRepository.java
@@ -4,5 +4,14 @@ import com.ravcontrol.backend.entity.ActivityGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ActivityGroupRepository  extends JpaRepository<ActivityGroup, Long> {}
+public interface ActivityGroupRepository  extends JpaRepository<ActivityGroup, Long> {
+
+    /**
+     * Esse método buscará todos os grupos no banco de dados ordenados em ordem ascendente
+     * @return Retorna uma lista de grupos de atividade
+     */
+    List<ActivityGroup> findAllByOrderByPositionAsc();
+}
